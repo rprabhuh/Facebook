7e00efbafbf7509c9e0cbfb0aa40bae6f11edb03
Rahul Prabhu <rprabhu@ufl.edu>
2015-11-30 21:08:00 -0500

    Made changes to the the data structures to enable basic authentication

--- src/main/scala/FBJsonProtocol.scala
+++ src/main/scala/FBJsonProtocol.scala
@@ -30,6 +30,7 @@ case class Experience (
 )
 
 case class Page (
+    var auth: String,
     id: String,
     var about: String,
     var can_post: Boolean,
@@ -54,20 +55,15 @@ case class Page (
 )
 
 case class Profile (
+    var auth: String,
     id: String,
-    //about: String,
     var bio: String,
     var birthday: String,
-    var education: Array[String],
     var email: String,
-    //favorite_athletes: Array[String],
-    //favorite_teams: Array[String],
     var first_name: String,
     var gender: String,
     var hometown: String,
-    //inspirational_people: Array[String],
     var interested_in: Array[String],
-    //is_verified: Boolean,
     var languages: Array[String],
     var last_name: String,
     var link: String,
@@ -77,16 +73,14 @@ case class Profile (
     var relationship_status: String,
     var religion: String,
     var significant_other: String,
-    //sports: Array[String],
-    //quotes: String,
     var updated_time: String,
     var website: String,
     var work: Array[String],
-    //var public_key: String,
     var cover: String
   )
 
 case class Status (
+    var auth: String,
     id: String,
     var created_time: String,
     var from: String,
@@ -104,6 +98,7 @@ case class FriendList (
 
 
 case class Album (
+    var auth: String,
     id: String,
     var count: Int,
     var cover_photo: String,
@@ -121,6 +116,7 @@ case class Album (
   )
 
 case class Photo (
+    var auth: String,
     id: String,
     var album: String,
     var created_time: String,
@@ -155,6 +151,7 @@ case class ObjectComments (
 )
 
 case class FriendReqest( 
+  var auth: String,
   fromid: String,
   toid: String
 )
@@ -163,15 +160,15 @@ case class ErrorClass(id: String, error: String)
 
 object FBJsonProtocol extends DefaultJsonProtocol with SprayJsonSupport {
 
-    implicit val albumFormat = jsonFormat14(Album)
-    implicit val pageFormat = jsonFormat21(Page.apply)
+    implicit val albumFormat = jsonFormat15(Album)
+    implicit val pageFormat = jsonFormat22(Page.apply)
     implicit val profileFormat = jsonFormat22(Profile.apply)
-    implicit val statusFormat = jsonFormat7(Status.apply)
+    implicit val statusFormat = jsonFormat8(Status.apply)
     implicit val friendListFormat = jsonFormat2(FriendList.apply)
     implicit val experienceFormat = jsonFormat5(Experience.apply)
-    implicit val photoFormat = jsonFormat12(Photo.apply)
+    implicit val photoFormat = jsonFormat13(Photo.apply)
     implicit val commentFormat = jsonFormat8(Comment.apply)
-    implicit val FriendReqestFormat = jsonFormat2(FriendReqest.apply)
+    implicit val FriendReqestFormat = jsonFormat3(FriendReqest.apply)
     implicit val objectCommentsFormat = jsonFormat4(ObjectComments.apply)
     implicit val errorFormat = jsonFormat2(ErrorClass.apply)
 
